05a0abe2b7d0226a120131d9bad15b060e1f9d63
Daniel Milkiewicz <daniel.milkiewicz@outlook.com>
2018-04-27 00:00:33 +0200

    Working release

--- js/script.js
+++ js/script.js
@@ -1,44 +1,48 @@
 "use strict"
 
-function usersApp() {
-    let firstName = document.getElementById("firstName");
-    let lastName = document.getElementById("lastName");
-    let submit = document.getElementById("submit");
-    let result = document.getElementById("result");
-    let buttonRemoveB = document.getElementById("buttonRemoveB");
-    let buttonRemoveE = document.getElementById("buttonRemoveE");
-    let buttonRemoveA = document.getElementById("buttonRemoveA");
-    let users = [];
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+const submit = document.getElementById("submit");
+const result = document.getElementById("result");
+const buttonRemoveB = document.getElementById("buttonRemoveB");
+const buttonRemoveE = document.getElementById("buttonRemoveE");
+const buttonRemoveA = document.getElementById("buttonRemoveA");
+const users = [];
 
-    class User {
-        constructor(firstName, lastName) {
-            this.firstName = firstName;
-            this.lastName = lastName;
-        }
-    }
+function User (firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+}
 
-    function makeUser() {
-        let user = new User(firstName, lastName);
-        return user;
-    }
+function makeUser() {
+    let user = new User(firstName.value, lastName.value);
+    showUsers(user);
+    return user;
+}
 
-    function addUser(user) {
-        users.push(user);
-    }
+function addUser(user) {
+    users.push(user);
+}
 
-    function removeUserB() {
-        users.shift();
-    }
+function showUsers(user) {
+    const text = document.createElement("p");
+    result.appendChild(text);
+    text.textContent += JSON.stringify(user.firstName) + " " + JSON.stringify(user.lastName);
+}
 
-    function removeUserE() {
-        users.pop();
-    }
+submit.addEventListener("click", () => {
+    addUser(makeUser());
+});
 
-    function removeUsers() {
-        users.length = 0;
-    }
+function removeUserB() {
+    result.children[0].remove();
+    users.shift();
+}
 
-    function showUsers() {
-        
-    }
+function removeUserE() {
+    users.pop();
+}
+
+function removeUsers() {
+    users.length = 0;
 }
