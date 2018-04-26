7f6fc2abd05a5b3dd10a219f47d9a7d4b114b155
Daniel Milkiewicz <daniel.milkiewicz@outlook.com>
2018-04-26 17:53:21 +0200

    Base website

--- js/script.js
+++ js/script.js
@@ -1,5 +1,44 @@
 "use strict"
 
-let rem = 3;
-let tette = "332";
-let test = 333;
+function usersApp() {
+    let firstName = document.getElementById("firstName");
+    let lastName = document.getElementById("lastName");
+    let submit = document.getElementById("submit");
+    let result = document.getElementById("result");
+    let buttonRemoveB = document.getElementById("buttonRemoveB");
+    let buttonRemoveE = document.getElementById("buttonRemoveE");
+    let buttonRemoveA = document.getElementById("buttonRemoveA");
+    let users = [];
+
+    class User {
+        constructor(firstName, lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+    }
+
+    function makeUser() {
+        let user = new User(firstName, lastName);
+        return user;
+    }
+
+    function addUser(user) {
+        users.push(user);
+    }
+
+    function removeUserB() {
+        users.shift();
+    }
+
+    function removeUserE() {
+        users.pop();
+    }
+
+    function removeUsers() {
+        users.length = 0;
+    }
+
+    function showUsers() {
+        
+    }
+}
