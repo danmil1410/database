57510a3bea761c0f85b8f4894afd679d376833b9
Daniel Milkiewicz <daniel.milkiewicz@outlook.com>
2018-05-07 19:18:58 +0200

    Fixes

---
+++ public/js/main.js
@@ -0,0 +1,95 @@
+"use strict";
+
+//Main module - app config
+const appConfigModule = (function() {
+    const _config = {
+        apiKey: "AIzaSyCWyU1e6WqN4dAFiQDuQG9pB69pSE1jPls",
+        authDomain: "database-66f5c.firebaseapp.com",
+        databaseURL: "https://database-66f5c.firebaseio.com",
+        projectId: "database-66f5c",
+        storageBucket: "database-66f5c.appspot.com",
+        messagingSenderId: "891831635853"
+    };
+    const _firestore = firebase.firestore();
+
+    firebase.initializeApp(_config);
+
+    return {
+        docRef: _firestore.collection("database")
+    };
+})();
+
+//Module - add a new user
+const addUserModule = (function() {
+    const _addButton = document.querySelector("#addUser");
+    const _firstName = document.querySelector("#firstName");
+    const _lastName = document.querySelector("#lastName");
+
+    _addButton.addEventListener("click", () => {
+        try {
+            if (_firstName.value && _lastName.value) {
+                appConfigModule.docRef
+                    .add({
+                        firstName: _firstName.value,
+                        lastName: _lastName.value
+                    })
+                    .then(() => {
+                        console.log("Data saved!");
+                    })
+                    .catch(error => {
+                        console.log("Error: ", error);
+                    });
+                _firstName.value = "";
+                _lastName.value = "";
+            } else {
+                throw new SyntaxError("Incomplete data: type the full name!");
+            }
+        } catch (e) {
+            alert(e.message);
+        }
+    });
+})();
+
+//Module - show existing users
+const showUsersModule = (function() {
+    const _showButton = document.querySelector("#showUsers");
+    const _resultContainer = document.querySelector("#result");
+
+    _showButton.addEventListener("click", () => {
+        appConfigModule.docRef.onSnapshot(querySnapshot => {
+            while (_resultContainer.firstChild) {
+                _resultContainer.removeChild(_resultContainer.firstChild);
+            }
+            querySnapshot.forEach(doc => {
+                const userData = doc.data();
+                const text = document.createElement("p");
+                _resultContainer.appendChild(text);
+                text.textContent =
+                    JSON.stringify(userData.firstName).replace(/["']/g, "") +
+                    " " +
+                    JSON.stringify(userData.lastName).replace(/["']/g, "");
+            });
+        });
+    });
+})();
+
+//Module - remove an user
+const removeUserModule = (function() {
+    const _removeButton = document.querySelector("#buttonRemove");
+    const _deleteFirstName = document.querySelector("#firstNameDelete");
+    const _deleteLastName = document.querySelector("#lastNameDelete");
+
+    _removeButton.addEventListener("click", () => {
+        appConfigModule.docRef.onSnapshot(querySnapshot => {
+            appConfigModule.docRef
+                .doc("rPese5BGn1jk2ih7zVnn")
+                .delete()
+                .then(() => {
+                    console.log("User saved!");
+                })
+                .catch(error => {
+                    console.log("Error: ", error);
+                });
+        });
+    });
+})();
